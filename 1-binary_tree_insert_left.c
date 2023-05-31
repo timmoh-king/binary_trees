0ebccccf3cef588471b388841d423538e25b2df6
timmoh-king <timothykariuki43@gmail.com>
2023-05-31 11:53:03 +0300

    binary insert right and left files

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - the left-child of another node
+ * @parent: the parent node
+ * @value: the value to be inserted in the left child
+ *
+ * Return: pointer to the left node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = binary_tree_node(parent, value);
+
+	if (parent == NULL)
+		return (NULL);
+	if (newNode == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
+	parent->left = newNode;
+	return (newNode);
+}
