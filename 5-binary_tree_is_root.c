d7944d6c27e67018614b0b35a316357084b2946a
timmoh-king <timothykariuki43@gmail.com>
2023-06-03 05:14:12 +0300

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,7 +13,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 
 	else
 	{
-	if (node->left != NULL || node->right != NULL)
+	if ((node->left != NULL && node->right != NULL) ||
+			(node->left != NULL || node->right != NULL))
 		return (1);
 	return (0);
 	}
